eabc8a715a619f95c3ed5372360cab8a082d72b2
Phil Ashby <phil.github@ashbysoft.com>
2020-08-04 13:33:00 +0100

    Updated deploy tooling, pass on webhook info

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,27 @@
 #! /bin/sh
 
 now=`date`
-echo -n "$now: "
+act="$GITHUB_EVENT"
+del="$GITHUB_DELIVERY"
+echo -n "deploy($act/$del)@$now: "
+
+# Skip unless act==push
+[ "$act" != "push" ] && echo "not push" && exit 0
+
 # Pull latest (sadly git pull doesn't have an exit code for 'up to date')
 res=`git pull`
-[ -z "$FORCE_HUGO" -a "$res" = "Already up to date." ] && exit 0
+[ -z "$FORCE_HUGO" -a "$res" = "Already up to date." ] && echo "up to date" && exit 0
 
 # Build for local server
-/usr/local/bin/hugo
+echo "building"
+
+/usr/local/bin/hugo || exit 1
 
 # Build for publication
-/usr/local/bin/hugo -b https://www.ashbysoft.com/ -d publish
+/usr/local/bin/hugo -b https://www.ashbysoft.com/ -d publish || exit 1
 
-# Push to Azure
-./upload-azure.py
-echo $now
+# Push to Azure (in background - or Github webhook times out)
+echo "building(done)@$now"
+echo "uploading (in background)"
+./upload-azure.py &
+exit 0
